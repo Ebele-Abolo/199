c0ba68f6940a9b2623dba5e8a75cecef89161480
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-07-20 14:59:14 -0500

    Add files via upload

---
+++ 199.py
@@ -0,0 +1,5 @@
+If(graph_type == 'Bubble'):
+  if(dataframe[yaxis].dtypes == 'int64'):
+ elif(graph_type == 'bar')
+ xlabel_widget = widgets.Dropdown(options = df.columns)
+ show_dataframe=widgets.Button(description="Show dataframe")
